d41ef8cb1e5909259c3d0293ffb9ef387c653598
Pep <163578295+disney-nat@users.noreply.github.com>
2024-04-22 19:08:19 +0200

    Update Joc.h

--- Joc.h
+++ Joc.h
@@ -1,5 +1,30 @@
-#pragma once
+v#ifndef JOC_H
+#define JOC_H
+#include "Tauler.h"
+#include "Figura.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
 class Joc
 {
+public:
+	Joc();
+	Joc(ColorFigura J_tauler);
+	void inicialitza(const string& nomFitxer);
+	bool giraFigura(DireccioGir direccio);
+	bool mouFigura(int dirX);
+	int baixaFigura();
+	void escriuTauler(const string& nomFitxer);
+	//fins aqui els que ens demanen a la primera entrega
+
+	int eliminarLinia();
+	void figuraTauler();
+private:
+	Tauler J_tauler;
+	Figura J_figura;
 };
 
+#endif
