ed53a4ce56034aee3aa97c237e06518c4e18da77
Pep <163578295+disney-nat@users.noreply.github.com>
2024-04-22 19:10:17 +0200

    Tauler.h amb errors

--- Tauler.h
+++ Tauler.h
@@ -1,5 +1,23 @@
-#pragma once
+#ifndef TAULER_H
+#define TAULER_H
+#include "Figura.h"
+
+const int MAX_FILA = 8;
+const int MAX_COL = 8;
+
 class Tauler
 {
+public:
+    //constructors
+    Tauler();
+    Tauler(ColorFigura taulerInicial[][MAX_COL]);
+    
+    //geters i seters
+    void setTauler(ColorFigura color, int i, int j) {T_tauler[i][j]=color;}
+    ColorFigura getTauler(int i, int j) {return T_tauler[i][j];}
+    void inicialitzarT(ColorFigura color[MAX_FILA][MAX_COL]);
+    
+private:
+    ColorFigura T_tauler[MAX_FILA][MAX_COL];
 };
-
+#endif
