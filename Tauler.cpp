5087674b76e3610125c9ad62d033c51e9bb0962d
Pep <163578295+disney-nat@users.noreply.github.com>
2024-04-22 19:10:46 +0200

    Tauler.cpp amb errors

--- Tauler.cpp
+++ Tauler.cpp
@@ -1,1 +1,27 @@
 #include "Tauler.h"
+
+Tauler::Tauler()
+{
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        for (int j = 0; j <MAX_COL; j++)
+        {
+            T_tauler[i][j]=NO_COLOR;
+        }
+    }
+}
+Tauler::Tauler(ColorFigura taulerInicial[][MAX_COL])
+{
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        for(int j = 0 ; j < MAX_COL; j++)
+        {
+            T_tauler[i][j]=taulerInicial[i][j];
+        }
+    }
+}
+
+void Tauler::inicialitzarT(ColorFigura color[MAX_FILA][MAX_COL])
+{
+    Tauler(color[MAX_FILA][MAX_COL]);
+}
