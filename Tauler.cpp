8f6b2dab3a03a5af75a471687c2f93fcc5dc0831
NIU1711491 <148430205+NIU1711491@users.noreply.github.com>
2024-06-05 17:39:54 +0200

    Update Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -2,26 +2,63 @@
 
 Tauler::Tauler()
 {
-    for (int i = 0; i < MAX_FILA; i++)
+    for (int i = 0; i < MAX_FILA ; i++)
     {
-        for (int j = 0; j <MAX_COL; j++)
+        for (int j = 1; j < MAX_COL+1; j++)
         {
-            T_tauler[i][j]=NO_COLOR;
+            T_tauler[i][j] = COLOR_NEGRE;
         }
     }
+
+    for (int j = 0; j < MAX_COL+2; j++)
+    {
+        T_tauler[8][j] = NO_COLOR;
+    }
+
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        T_tauler[i][0] = NO_COLOR;
+    }
+
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        T_tauler[i][9] = NO_COLOR;
+    }
 }
+void Tauler::inicialitzarT()
+{
+    Tauler();
+}
+
 Tauler::Tauler(ColorFigura taulerInicial[][MAX_COL])
 {
     for (int i = 0; i < MAX_FILA; i++)
     {
-        for(int j = 0 ; j < MAX_COL; j++)
+        for (int j = 0; j < MAX_COL; j++)
         {
-            T_tauler[i][j]=taulerInicial[i][j];
+            T_tauler[i][j] = taulerInicial[i][j];
         }
     }
 }
 
-void Tauler::inicialitzarT(ColorFigura color[MAX_FILA][MAX_COL])
+void Tauler::inicialitzarT(int color[MAX_FILA][MAX_COL])
 {
-    Tauler(color[MAX_FILA][MAX_COL]);
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        for (int j = 1; j < MAX_COL+1; j++)
+        {
+            switch (color[i][j-1])
+            {
+            case 0: T_tauler[i][j] = COLOR_NEGRE; break;
+            case 1: T_tauler[i][j] = COLOR_GROC; break;
+            case 2: T_tauler[i][j] = COLOR_BLAUCEL; break;
+            case 3: T_tauler[i][j] = COLOR_MAGENTA; break;
+            case 4: T_tauler[i][j] = COLOR_TARONJA; break;
+            case 5: T_tauler[i][j] = COLOR_BLAUFOSC; break;
+            case 6: T_tauler[i][j] = COLOR_VERMELL; break;
+            case 7: T_tauler[i][j] = COLOR_VERD; break;
+            default: T_tauler[i][j] = NO_COLOR; break;
+            }
+        }
+    }
 }
