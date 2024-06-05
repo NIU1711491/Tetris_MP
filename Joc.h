a7b44a6bdbd0ea64e06548b63ed218321f00da55
NIU1711491 <148430205+NIU1711491@users.noreply.github.com>
2024-06-05 17:38:20 +0200

    Joc.h sense errors

--- Joc.h
+++ Joc.h
@@ -1,10 +1,14 @@
-v#ifndef JOC_H
+#ifndef JOC_H
 #define JOC_H
 #include "Tauler.h"
 #include "Figura.h"
 #include <string>
 #include <fstream>
 #include <iostream>
+#include "Partida.h"
+#include "InfoJoc.h"
+#include "GraphicManager.h"
+
 
 using namespace std;
 
@@ -12,13 +16,21 @@ class Joc
 {
 public:
 	Joc();
-	Joc(ColorFigura J_tauler);
 	void inicialitza(const string& nomFitxer);
 	bool giraFigura(DireccioGir direccio);
 	bool mouFigura(int dirX);
 	int baixaFigura();
 	void escriuTauler(const string& nomFitxer);
 	//fins aqui els que ens demanen a la primera entrega
+
+	void deleteFigura();
+	bool jocAcabat();
+	void generarFigura();
+	void dibuixarJoc();
+
+	bool potMoure(int dirX);
+	bool potBaixar();
+	bool potGirar(DireccioGir direccio);
 
 	int eliminarLinia();
 	void figuraTauler();
